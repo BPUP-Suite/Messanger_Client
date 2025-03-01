545562c2fd30b143f3f2bfa23cb9e272885632da
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2025-03-01 20:32:49 +0000

    Nuova versione compatibile solo per Windows

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Esponi l'API per il renderer (pulsanti)
-contextBridge.exposeInMainWorld('ipcRender', {
-  send: (channel, data) => ipcRenderer.send(channel, data),
-});
